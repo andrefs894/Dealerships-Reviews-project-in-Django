c7d19869b05b66804ea5ccbe4bb2f7f76e28bfff
André <andrefs894@gmail.com>
2023-11-14 19:11:29 -0500

    changes made

---
+++ functions/get-review.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const Cloudant = require('@cloudant/cloudant');
+
+// Initialize Cloudant connection with IAM authentication
+async function dbCloudantConnect() {
+    try {
+        const cloudant = Cloudant({
+            plugins: { iamauth: { iamApiKey: 'uFeWPUC_w6Qg79y295QN_8TS6Pd8WBAoj9QvIFbSPFBC' } }, // Replace with your IAM API key
+            url: 'https://b69568f1-7faa-40da-a28a-905167584d3d-bluemix.cloudantnosqldb.appdomain.cloud', // Replace with your Cloudant URL
+        });
+
+        const db = cloudant.use('reviews');
+        console.info('Connect success! Connected to DB');
+        return db;
+    } catch (err) {
+        console.error('Connect failure: ' + err.message + ' for Cloudant DB');
+        throw err;
+    }
+}
+
+let db;
+
+(async () => {
+    db = await dbCloudantConnect();
+})();
+
+app.use(express.json());
+
+// Define a route to get all dealerships with optional state and ID filters
+app.get('/reviews/get', (req, res) => {
+    const { state, id } = req.query;
+
+    // Create a selector object based on query parameters
+    const selector = {};
+    if (state) {
+        selector.state = state;
+    }
+    
+    if (id) {
+        selector.id = parseInt(id); // Filter by "id" with a value of 1
+    }
+
+    const queryOptions = {
+        selector,
+        limit: 10, // Limit the number of documents returned to 10
+    };
+
+    db.find(queryOptions, (err, body) => {
+        if (err) {
+            console.error('Error fetching dealerships:', err);
+            res.status(500).json({ error: 'An error occurred while fetching dealerships.' });
+        } else {
+            const dealerships = body.docs;
+            res.json(dealerships);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
